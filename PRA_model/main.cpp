9ae43952943ae24c189a09464a82ea98cb9c93fd
AshBringer47 <31385699+AshBringer47@users.noreply.github.com>
2019-10-14 00:26:38 +0300

    Update main.cpp
    Took away std:: by adding "using namespace std".

--- PRA_model/main.cpp
+++ PRA_model/main.cpp
@@ -8,29 +8,31 @@
 #include <memory>
 #include <thread>
 
+using namespace std
+
 int main()
 {
     ram_ptr ram = ram_ptr(new RandomAccesMemory(constants::RAM_PAGE_COUNT));
 
     mmu_ptr mmu = mmu_ptr(new PagedMemoryManagementUnit(ram));
 
-    std::shared_ptr<ProcessQueue> queue = std::shared_ptr<ProcessQueue>(new ProcessQueue(mmu));
+   shared_ptr<ProcessQueue> queue = shared_ptr<ProcessQueue>(new ProcessQueue(mmu));
     mmu->setProcessContainer(queue);
 
     for(int i = 0; i < 30; i++) {
         queue->addProcess(process_ptr(new Program()));
     }
 
-    std::cin.get();
-    std::cout << "Run\n";
+    cin.get();
+    cout << "Run\n";
 
     queue->run();
 
     while(true) {
-//        std::cout << mmu->getPageFaultCount() << "\n";
+//        cout << mmu->getPageFaultCount() << "\n";
     }
 
-    std::cin.get();
+    cin.get();
 
     return 0;
 }
